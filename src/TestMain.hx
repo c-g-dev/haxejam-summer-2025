3ff7731e1456dbf333d04c790c3f4a7bf41bae12
cardgdev <cardgamedev@proton.me>
2025-08-05 20:52:32 -0500

    f

--- src/TestMain.hx
+++ src/TestMain.hx
@@ -7,10 +7,10 @@ class TestMain extends hxd.App {
     override function init() {
         hxd.Res.initEmbed();
 
-        var test = new h2d.Bitmap(hxd.Res.sprite_placeholder.toTile());
+        var test = new h2d.Bitmap(hxd.Res.guy.toTile());
         test.x = 100;
         test.y = 100;
-        var ghost = new GhostTrailCentered(test, () -> {return {dx: 5 * Math.sin(Timer.stamp()), dy: 5 * Math.cos(Timer.stamp())};});
+        var ghost = new GhostTrailCentered(test, () -> {return {dx: 10, dy: 0};});
         ghost.x = 100;
         ghost.y = 100;
         s2d.addChild(ghost);
